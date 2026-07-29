f2f84ceaf7f37b7ecbca9f66b798cf83fbc0595b
alithemenace-create <aliinfigonfutures@gmail.com>
2026-07-29 10:54:20 +0530

    Update Appointment.js

--- src/models/Appointment.js
+++ src/models/Appointment.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const appointmentSchema = new mongoose.Schema({
   name:      { type: String, required: true },
   email:     { type: String, default: '' },
-  phone:     { type: String, required: true },
+  phone:     { type: String, default: '' },
   date:      { type: String, required: true },
   time:      { type: String, required: true },
   focus:     { type: String, default: '' },
